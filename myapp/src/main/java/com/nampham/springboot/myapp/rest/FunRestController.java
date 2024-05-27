3727045082b2a5dfd6158947f34b5da13d7da23d
GNLimerence <phamhoanghainam203@gmail.com>
2024-05-28 03:07:09 +0700

    28/5

--- myapp/src/main/java/com/nampham/springboot/myapp/rest/FunRestController.java
+++ myapp/src/main/java/com/nampham/springboot/myapp/rest/FunRestController.java
@@ -1,10 +1,27 @@
 package com.nampham.springboot.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose info
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
